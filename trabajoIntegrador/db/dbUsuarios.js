a1336716e78225e5379487a957e0abe48adde04b
Stefanocavallo <cavallos@udesa.edu.ar>
2024-04-15 09:24:56 -0300

    db de usuarios

---
+++ trabajoIntegrador/db/dbUsuarios.js
@@ -0,0 +1,51 @@
+const dataU = {
+    usuarios: [
+        {
+            id: 1,
+            usuario: 'Tefa123',
+            email: 'cavallos@udesa.edu.ar',
+            contrasenia: 'tefa123',
+            fechaNacimiento: '2003-07-21',
+            numeroDocumento: 44895188,
+            foto: 'fotoPerfilTefa.png',
+        },
+        {
+            id: 2,
+            usuario: 'Pepo123',
+            email: 'planusmartinez@udesa.edu.ar',
+            contrasenia: 'pepo123',
+            fechaNacimiento: '2004-12-08',
+            numeroDocumento: 46293905,
+            foto: 'fotoPerfilPepo.png',
+        },
+        {
+            id: 3,
+            usuario: 'Messi123',
+            email: 'lionelmessi@gmail.com',
+            contrasenia: 'messi123',
+            fechaNacimiento: '1987-06-24',
+            numeroDocumento: 18122022,
+            foto: 'fotoPerfilMessi.png',
+        },
+        {
+            id: 4,
+            usuario: 'Scaloni123',
+            email: 'scaloni@gmail.com',
+            contrasenia: 'scaloni123',
+            fechaNacimiento: '1978-05-16',
+            numeroDocumento: 20122022,
+            foto: 'fotoPerfilScaloni.png',
+        },
+        {
+            id: 5,
+            usuario: 'Ivan123',
+            email: 'idivinskymolina@udesa.edu.ar',
+            contrasenia: 'ivan123',
+            fechaNacimiento: '2004-12-21',
+            numeroDocumento: 46437670,
+            foto: 'fotoPerfilIvan.png',
+        },
+    ],
+};
+
+module.exports = dbUsuarios;
