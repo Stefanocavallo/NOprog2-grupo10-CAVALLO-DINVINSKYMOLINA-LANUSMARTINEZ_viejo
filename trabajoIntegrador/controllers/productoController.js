a519b6f58262d7e83d12fed79de6625be9b88871
Stefanocavallo <cavallos@udesa.edu.ar>
2024-06-16 17:44:15 -0300

    Modelo producto y relaciones

--- trabajoIntegrador/controllers/productoController.js
+++ trabajoIntegrador/controllers/productoController.js
@@ -1,20 +1,20 @@
-const dbProductos = require('../db/dbProductos')
+const db = require('../database/models')
 const express = require("express")
 
 const productosController = {
     index: function(req,res){
-        return res.render("index", {info: dbProductos.productos})
+        return res.render("index", {info: db.productos})
     },
     product: function(req, res){
         let id = req.params.idProduct;
         let respuesta;
-        for (let i = 0; i < dbProductos.productos.length; i++) {
-            if (id.toLowerCase() === dbProductos.productos[i].nombreProducto.toLowerCase()) {
-                respuesta = dbProductos.productos[i]
+        for (let i = 0; i < db.productos.length; i++) {
+            if (id.toLowerCase() === db.productos[i].nombreProducto.toLowerCase()) {
+                respuesta = db.productos[i]
             }
         }
         if (!respuesta) {
-            return res.send("El producto seleccionado no está disponible")
+            return res.send("El vehículo seleccionado no está disponible")
         }
         else {
             return res.render("product", {info: respuesta})
@@ -29,9 +29,9 @@ const productosController = {
         { 
             let buscado= req.query.search
             let rta=[]
-            for (let i = 0; i < dbProductos.productos.length; i++) {
-                if (dbProductos.productos[i].nombreProducto.toLowerCase().includes(buscado.toLowerCase()) ) {
-                    rta.push(dbProductos.productos[i])}}
+            for (let i = 0; i < db.productos.length; i++) {
+                if (db.productos[i].nombreProducto.toLowerCase().includes(buscado.toLowerCase()) ) {
+                    rta.push(db.productos[i])}}
             return res.render('searchResults',{info:rta})
                 }
         }
