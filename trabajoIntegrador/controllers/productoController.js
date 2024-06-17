74efedaa9b9cdd54fb599a6a704d7e3b2834103b
Stefanocavallo <cavallos@udesa.edu.ar>
2024-06-16 23:56:36 -0300

    vuelta atras por error y confusion

--- trabajoIntegrador/controllers/productoController.js
+++ trabajoIntegrador/controllers/productoController.js
@@ -1,42 +1,37 @@
-const db = require('../database/models')
+const dbProductos = require('../db/dbProductos')
 const express = require("express")
 
 const productosController = {
     index: function(req,res){
-        return res.render("index", {info: db.productos})
+        return res.render("index", {info: dbProductos.productos})
     },
     product: function(req, res){
         let id = req.params.idProduct;
         let respuesta;
-        for (let i = 0; i < db.productos.length; i++) {
-            if (id.toLowerCase() === db.productos[i].nombreProducto.toLowerCase()) {
-                respuesta = db.productos[i]
+        for (let i = 0; i < dbProductos.productos.length; i++) {
+            if (id.toLowerCase() === dbProductos.productos[i].nombreProducto.toLowerCase()) {
+                respuesta = dbProductos.productos[i]
             }
         }
         if (!respuesta) {
-            return res.send("El vehículo seleccionado no está disponible")
+            return res.send("El producto seleccionado no está disponible")
         }
         else {
             return res.render("product", {info: respuesta})
         }
     },
-
     add: function(req, res){
         return res.render('productAdd',{})
     },
-
     search: function(req, res){
         { 
             let buscado= req.query.search
             let rta=[]
-            for (let i = 0; i < db.productos.length; i++) {
-                if (db.productos[i].nombreProducto.toLowerCase().includes(buscado.toLowerCase()) ) {
-                    rta.push(db.productos[i])}}
+            for (let i = 0; i < dbProductos.productos.length; i++) {
+                if (dbProductos.productos[i].nombreProducto.toLowerCase().includes(buscado.toLowerCase()) ) {
+                    rta.push(dbProductos.productos[i])}}
             return res.render('searchResults',{info:rta})
                 }
         }
 }
-
-
 module.exports = productosController
-
